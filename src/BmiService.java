67526b8fcfd8bb26f56c6097f73b90e5929657b4
Кухоцкая Екатерина <ekukhotskaya@mail.ru>
2022-05-29 18:03:32 +0300

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,6 @@
 public class BmiService {
-    public double calculate(double weight, int hight) {
-
-        double bmiHuman = weight / (hight * hight / 10_000);  // делим на 10 000, потому что рост в сантиметрах.
+    public double calculate(double weight, int height) {
+        double bmiHuman = weight / (height * height / 10_000);  // делим на 10 000, потому что рост в сантиметрах.
         return bmiHuman;
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double weight = 85;   // Задаем значение веса (кг)
-        int  height = 182;  // Задаем значение роста в см (чаще люди помнят рост в сантиметрах)
-        if ((height <= 0)|(weight <= 0)) {
+        int height = 182;  // Задаем значение роста в см (чаще люди помнят рост в сантиметрах)
+        if ((height <= 0) | (weight <= 0)) {
             System.out.println("Расчет невозможен при нулевом или отрицательном значении веса или роста");
         } else {
             double bmi = Math.floor(service.calculate(weight, height));
